ba619984283027860dc6027ff961c86946275504
123456789 <4545454as@1qq.sdza>
2024-03-05 13:05:04 +0100

    translation update

---
+++ lang/zh-tw/settings.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author 123456789 <4545454as@1qq.sdza>
+ */
+$lang['sql_host']              = 'SQL 主機';
+$lang['sql_user']              = 'SQL 帳戶';
+$lang['sql_pass']              = 'SQL 密碼';
+$lang['database']              = 'AuthMe 資料庫名稱';
+$lang['table']                 = 'AuthMe 表名稱';
+$lang['admin']                 = '管理員帳戶名稱';
